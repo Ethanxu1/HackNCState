e676995828971b09086f486ac587dd7a75f907c1
Mihika Tyagi <156344471+mihikaty@users.noreply.github.com>
2025-02-09 07:37:25 -0500

    Changed colors of HomeScreen AppBar

--- piggybankapp/lib/home_screen.dart
+++ piggybankapp/lib/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:piggybankapp/connections_screen.dart';
 import 'package:piggybankapp/profile_screen.dart';
 import 'package:provider/provider.dart';
 import 'login_backend.dart';
@@ -12,7 +13,8 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('PiggyBank'),
+        title: const Text('PiggyBank', style: TextStyle(color: Color.fromRGBO(255,100,112, 1.0) )),
+        backgroundColor: Color.fromRGBO(2, 131, 217, 1.0),
         actions: [
           IconButton(
             icon: const Icon(Icons.logout),
@@ -44,7 +46,7 @@ class HomeScreen extends StatelessWidget {
             title: const Text('Connections'),
             leading: const Icon(Icons.group),
             onTap: () => Navigator.push(context,
-                MaterialPageRoute(builder: (_) => ProfileScreen())),
+                MaterialPageRoute(builder: (_) => ConnectionsScreen())),
           ),
         ],
       ),
